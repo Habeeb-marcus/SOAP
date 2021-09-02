bc71064018a86acc1ae8bd04f326baf24ed120e7
mac <h.makusota@outlook.com>
2021-09-02 15:17:29 +0100

    cartNumber localStorage

--- app.js
+++ app.js
@@ -6,9 +6,64 @@ const homePage = document.querySelector('#land')
 const logIn = document.querySelector('#login')
 const catalog = document.getElementById('catalog');
 const catalogCnt = document.querySelector('.catalog-cont');
+const products = document.querySelector('.products');
+
+const cartBtn = document.querySelectorAll('.add-cart');
+const cartItem = document.querySelector('.cart-item');
+
+
+// ui for catalog
+// prd-title, add-cart, price, img-card
+
+const imgCard = document.querySelector('img-card');
+const price = document.querySelector('price');
+const addCart = document.querySelector('add-cart');
+const prdTitle = document.querySelector('prd-title');
 
 
 
+let cartNumber = 0;
+
+cartBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+            // cartNumber ++;
+            // cartItem.innerHTML = cartNumber
+            cartNumbers()
+            
+    })
+} )
+
+function onLoadCartNumbers() {
+    let productsNumbers = localStorage.getItem('cartNumbers');
+
+    if(productsNumbers) {
+        cartItem.textContent = productsNumbers;
+    }
+}
+
+function cartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    //  console.log(productNumbers);
+    productNumbers = parseInt(productNumbers);
+      
+    if( productNumbers ) {
+         localStorage.setItem('cartNumbers', productNumbers + 1);
+         cartItem.textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        cartItem.textContent = 1;
+    }
+
+}
+
+
+
+
+
+
+// cartBtn.
+var hidden = true
 
 // Array.from(btn).forEach(function(btnEl){
 //     for(var i = -800; i <= -1600; i++)
@@ -74,7 +129,6 @@ for(let i = 0; i < nav.length; i++) {
        navLinks[i].classList.add('active-link');      
         }     
     )}
-
 
 
 
@@ -166,3 +220,118 @@ catalog.addEventListener('click', showCatalog)
 
 // }
 
+
+
+//getting our products
+
+async function getProducts() {
+    try {
+        let result = await fetch('./products.json');
+        let data = await result.json();
+        const productCards = data
+        
+        console.log(data);
+        // console.log(productCards);
+
+        // populate the catalog page
+ // return productCards;
+  
+        // productCards.forEach(product => {
+        //     // imgCard.textContent.src = 'data.url';
+        //     prdTitle.textContent = 'product.product-title';
+        //     price.innerHTML = 'product.price';
+            
+        // })
+        
+        // return productCards;
+
+    }catch(error) {
+        console.log('Product not fetched', error);
+    }
+    
+};
+
+getProducts()
+// console.log(result);
+// console.log(productCards);
+
+
+
+
+// pagination
+const params = {
+    numberOfCards: products.children,
+    cardsPerPage : 6,
+    currentPage : 4,
+    numberOfButtons : 4
+};
+
+
+// console.log(products.children);
+const createPagination = (params) => {
+    const {
+        numberOfCards,
+        cardsPerPage,
+        currentPage,
+        numberOfButtons 
+    } = params;
+
+    const numberOfPages = Math.ceil(numberOfCards / cardsPerPage);
+
+    if(currentPage > numberOfPages || currentPage < 1) return {
+        pagination: [],
+        currentPage
+    }
+
+
+    const buttons = Array(numberOfPages).fill(1 ).map((e, i) => e + i);
+    const sideButtons = numberOfButtons % 2 === 0 ? numberOfButtons/2 : (numberOfButtons - 1);
+
+
+    console.log(sideButtons)
+    console.log(buttons)
+    const calculLeft =  (rest = 0) => {
+        return {
+            array : buttons
+            .slice(0, currentPage - 1)
+            .reverse()
+            .slice(0, sideButtons + rest)
+            .reverse(),   
+
+            rest: function () {
+               return sideButtons - this.array.length
+            }
+        };
+    };
+
+
+    const calculRight =  (rest = 0) => {
+        return {
+            array : buttons
+            .slice(currentPage)
+            .slice(0, sideButtons + rest),
+            
+            rest: function () {
+               return sideButtons - this.array.length;
+            }
+        };
+    };
+
+    const leftButton = calculLeft(calculRight().rest()).array;
+    const rightButton = calculRight(calculLeft().rest()).array;
+
+    return {
+        pagination: [...leftButton, "current page", ...rightButton],
+        currentPage
+    }
+
+    // return {leftButton, rightButton}
+    return ; 
+}
+
+
+// const pagination = createPagination(params);
+
+// console.log(pagination);
+
+onLoadCartNumbers();
